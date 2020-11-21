61d76e98195d8769dd7b3027f487f0d82bb3abb9
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-11-21 03:46:21 -0700

    Add create_app() and app.run() statements

--- backend/src/app.py
+++ backend/src/app.py
@@ -19,3 +19,11 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+
+    return app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
