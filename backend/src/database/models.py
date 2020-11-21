9408ba8862f19d2683e1f94b233e407dee8f0536
Jonathan <THEJAGSTER@Jagsters-Mac.local>
2020-11-21 04:04:09 -0700

    Add setup_db() and comments

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,3 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+# ----------------------------------------------------------------------------#
+# setup_db(app)
+#     binds a flask application and a SQLAlchemy service
+# ----------------------------------------------------------------------------#
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
