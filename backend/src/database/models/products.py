3053cc362166a6ced6e77ca7f76eb5ad43297474
Jonathan <jonguti23@outlook.com>
2020-11-23 11:39:54 -0700

    Move and update files

--- backend/src/database/models/products.py
+++ backend/src/database/models/products.py
@@ -1,3 +1,7 @@
+from app import db
+from sqlalchemy import Column, Integer, String, Float, create_engine
+from flask_sqlalchemy import SQLAlchemy
+
 # TODO Create 'Products' model
 
 '''
@@ -31,3 +35,42 @@ The Product Model columns are:
     > *Foreign Key*: ReviewID <INTEGER> (This Model may include Ratings)
     - Note <STRING> [Limit 255] (e.g. Availability between October - December)
 '''
+
+
+class Products(db.Model):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    product_name = Column(String(60), unique=True, nullable=False)
+    product_description = Column(String(255), nullable=False)
+    msrp = Column(Float, nullable=False)
+    picture = Column(String(50))
+    # TODO add foreign keys
+
+    def __init__(self, product_name, product_description, msrp, picture):
+        # Example: self.question = question
+        #          self.answer = answer
+        self.product_name = product_name
+        self.product_description = product_description
+        self.msrp = msrp
+        self.picture = picture
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'product_name': self.product_name,
+            'product_description': self.product_description,
+            'msrp': self.msrp,
+            'picture': self.picture
+        }
