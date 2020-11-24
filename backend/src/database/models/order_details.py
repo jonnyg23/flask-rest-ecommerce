6c4b6ed5a57f543a7d0761270c381d7655616a4a
Jonathan <jonguti23@outlook.com>
2020-11-24 06:37:25 -0700

    Create Order_Details Model & class methods

--- backend/src/database/models/order_details.py
+++ backend/src/database/models/order_details.py
@@ -1,4 +1,8 @@
-# TODO Create 'OrderDetails' model
+from app import db
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, \
+    Float, create_engine
+from flask_sqlalchemy import SQLAlchemy
+
 
 '''
 The OrderDetails Model columns are:
@@ -21,3 +25,46 @@ The OrderDetails Model columns are:
     - Freight <FLOAT> (Freight charges if shipped altogether)
     - SalesTax <FLOAT> (Sales tax on entire order)
 '''
+
+
+class Order_Details(db.Model):
+    __tablename__ = 'order_details'
+
+    id = Column(Integer, primary_key=True)
+    price = Column(Float, nullable=False)
+    quantity = Column(Integer, nullable=False)
+    discount = Column(Float, nullable=False)
+    total = Column(Float, nullable=False)
+    fulfilled = Column(Boolean, nullable=False)
+    bill_date = Column(DateTime, nullable=False)
+    # TODO add foreign keys
+
+    def __init__(self, price, quantity, discount, total, fulfilled, bill_date):
+        self.price = price
+        self.quantity = quantity
+        self.discount = discount
+        self.total = total
+        self.fulfilled = fulfilled
+        self.bill_date = bill_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'quantity': self.quantity,
+            'discount': self.discount,
+            'total': self.total,
+            'fulfilled': self.fulfilled,
+            'bill_date': self.bill_date
+        }
