373727ca16a5bdb402c551be27204cb897d0cac6
Jonathan <jonguti23@outlook.com>
2020-11-30 23:19:34 -0700

    Remove sqlalchemy 'DateTime' dependency

--- backend/src/models/suppliers.py
+++ backend/src/models/suppliers.py
@@ -1,5 +1,5 @@
 from app import db
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, \
+from sqlalchemy import Column, Integer, String, Boolean, \
     Float, create_engine, relationship
 from flask_sqlalchemy import SQLAlchemy
 
