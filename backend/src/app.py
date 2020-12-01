a2bc0b575bfda955cfd8444a42513ebac12cdfaa
Jonathan <jonguti23@outlook.com>
2020-11-30 23:36:01 -0700

    Added error handler methods

--- backend/src/app.py
+++ backend/src/app.py
@@ -38,6 +38,65 @@ def create_app(test_config=None):
 
     # TODO Add '/collections/gadgets' endpoint
 
+
+# ----------------------------------------------------------------------------#
+# App Error Handling
+# ----------------------------------------------------------------------------#
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        """400 Bad-Request App Error Handler"""
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        """401 Unauthorized App Error Handler"""
+        return jsonify({
+            "success": False,
+            "error": 401,
+            "message": "unauthorized"
+        }), 401
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        """403 Forbidden App Error Handler"""
+        return jsonify({
+            "success": False,
+            "error": 403,
+            "message": "forbidden"
+        }), 403
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """404 Not-Found App Error Handler"""
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        """422 Unprocessable App Error Handler"""
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        """500 Internal Server Error App Error Handler"""
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'internal server error'
+        }), 500
+
     return app
 
 
