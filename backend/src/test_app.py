ff47fcc43efd7044eaef29e6b188162c2ecea249
Jonathan <jonguti23@outlook.com>
2020-12-01 23:23:02 -0700

    Add environ vars & setUp() method

--- backend/src/test_app.py
+++ backend/src/test_app.py
@@ -13,6 +13,24 @@ from .models.payment import Payment
 from .models.shippers import shippers
 from .models.suppliers import suppliers
 
+database_name = os.environ['DATABASE_NAME']
+database_path = os.environ['DATABASE_URL']
+
 
 class EcommerceTestCase(unittest.TestCase):
     """This class represents the E-commerce Test Case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = database_name
+        self.database_path = database_path
+        setup_db(self.app, self.database_path)
+
+        # Binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # Create all tables
+            self.db.create_all()
