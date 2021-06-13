98cdadecb951a3c8a651367d89b39ecd96d79537
jonnyg23 <jonguti23@outlook.com>
2021-06-13 00:20:58 -0600

    Add axios test on home page

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,35 @@
-import React, { Fragment } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import Welcome from "../components/welcome";
+import useAxios from "../hooks/useAxios";
+import { backendApi } from "../apis/axiosRequests";
 
-const Home = () => (
-  <Fragment>
-    <Welcome />
-  </Fragment>
-);
+const Home = () => {
+  const [url, setUrl] = useState("/products");
+
+  const { response, isLoading } = useAxios({
+    api: backendApi,
+    method: "get",
+    url: `${url}`
+    // config: JSON.stringify({ requireAuthentication: true }),
+  });
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (response !== null) {
+      setData(response);
+    }
+  }, [response]);
+
+  return (
+    <Fragment>
+      <Welcome />
+      <button onClick={() => setUrl("/collections/holiday")}>Holiday</button>
+      <button>Products</button>
+
+      <div>{JSON.stringify(data)}</div>
+    </Fragment>
+  );
+};
 
 export default Home;
