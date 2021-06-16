bdb0966a5a9b87615530414d3b415c17e2ce0b04
jonnyg23 <jonguti23@outlook.com>
2021-06-16 00:15:09 -0600

    Update welcome statement

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -6,10 +6,6 @@ const Welcome = () => (
   <Box>
     <Typography variant="h3">Welcome to Flask-Ecommerce!</Typography>
     <Typography variant="h5" gutterBottom>
-      Frontend UI is currently under development. However, the backend API is
-      functional.
-    </Typography>
-    <Typography variant="body1" gutterBottom>
       This is the Flask-Ecommerce REST Website template.
     </Typography>
   </Box>
