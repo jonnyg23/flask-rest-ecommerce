797a3804c7658b6478f155e3915ca9416a9e2835
jonnyg23 <jonguti23@outlook.com>
2021-06-17 00:51:11 -0600

    Add history to router

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import App from "./app";
 import { BrowserRouter as Router } from "react-router-dom";
 import ThemeModeProvider from './context/ThemeModeProvider';
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import history from "./history"
 
-import "./index.css";
 
 if (module.hot) {
   module.hot.accept();
 }
 
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <ThemeModeProvider>
       <Auth0ProviderWithHistory>
         <App />
       </Auth0ProviderWithHistory>
     </ThemeModeProvider>
   </Router>,
-  document.getElementById("root")
+  document.querySelector("#root")
 );
