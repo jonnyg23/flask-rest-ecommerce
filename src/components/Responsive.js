6e610448e82966cb0e15a1f3f6a3273061759da2
jonnyg23 <jonguti23@outlook.com>
2021-06-16 22:08:40 -0600

    Init Responsive mediaQueries

---
+++ src/components/Responsive.js
@@ -0,0 +1,27 @@
+import { useMediaQuery } from "react-responsive";
+
+export const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 992 });
+  return isDesktop ? children : null;
+};
+
+export const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+  return isTablet ? children : null;
+};
+
+export const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
+
+export const SmallScreen = ({ children }) => {
+  const isSmallScreen = useMediaQuery({ maxWidth: 991 });
+  return isSmallScreen ? children : null;
+};
+
+export const isPortrait = () =>
+  useMediaQuery({ query: "(orientation: portrait)" });
+
+export const isRetina = () =>
+  useMediaQuery({ query: "(min-resolution: 2dppx)" });
