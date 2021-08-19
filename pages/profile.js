59390bcfd39169349daf76eab4a223cd32072b8d
jonnyg23 <jonguti23@outlook.com>
2021-08-19 13:59:06 -0600

    Add Auth0 page auth requirement

--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,14 @@
-import React, { useState} from "react";
-import auth0 from "../hooks/auth0";
-import { WithPageAuthRequired } from "@auth0/nextjs-auth0/dist/frontend";
+import React from "react";
+import Link from "next/link";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import setInitTheme from "../hooks/setInitTheme";
-import { Box, Typography, Paper, Avatar } from "@material-ui/core";
+import {
+  Box,
+  Typography,
+  Paper,
+  Avatar,
+  CircularProgress,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { teal } from "@material-ui/core/colors";
 import JSONPretty from "react-json-pretty";
@@ -29,53 +35,66 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Profile = ({ initialAppTheme, user }) => {
+const Profile = ({ initialAppTheme }) => {
   setInitTheme(initialAppTheme);
   const classes = useStyles();
-  const [bearer_token, getBearerToken] = useState("");
 
-  // useEffect(() => {
-  //   getBearerToken(document.cookie);
-  // }, []);
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading)
+    return (
+      <>
+        <Layout></Layout>
+        <CircularProgress color="primary" />
+      </>
+    );
+
+  if (error) return <div>{error.message}</div>;
+
+  if (!user)
+    return (
+      <Link href="/api/auth/login">
+        <a>Login</a>
+      </Link>
+    );
 
   return (
     <>
-      <WithPageAuthRequired>
-        <Layout>
-          <Box>
-            {user.picture ? (
-              <div className={classes.image}>
-                <Avatar
-                  variant="circle"
-                  alt="Profile"
-                  src={user.picture}
-                  className={classes.large}
-                />
-              </div>
-            ) : null}
-            <Box mb={3}>
-              <Typography variant="h2">{user.name}</Typography>
-              <Typography variant="body1">{user.nickname}</Typography>
-            </Box>
+      <Layout>
+        <Box>
+          {user.picture ? (
+            <div className={classes.image}>
+              <Avatar
+                variant="circle"
+                alt="Profile"
+                src={user.picture}
+                className={classes.large}
+              />
+            </div>
+          ) : null}
+          <Box mb={3}>
+            <Typography variant="h2">{user.name}</Typography>
+            <Typography variant="body1">Nickname: {user.nickname}</Typography>
+          </Box>
 
-            <Paper elevation={3}>
-              <Box padding={2}>
-                {/* <JSONPretty
+          {/* <Paper elevation={3}>
+            <Box padding={2}>
+              <JSONPretty
                   id="user-bearer-token"
                   data={bearer_token}
                   style={{
                     overflow: "auto",
                   }}
-                ></JSONPretty> */}
-              </Box>
-            </Paper>
-
-            <Box mt={3} mb={1}>
-              <Typography variant="body1">
-                Click below to view your unique Bearer JWT Access Token:
-              </Typography>
+                ></JSONPretty>
             </Box>
-            {/* <Box mb={2}>
+          </Paper>
+
+          <Box mt={3} mb={1}>
+            <Typography variant="body1">
+              Click below to view your unique Bearer JWT Access Token:
+            </Typography>
+          </Box>
+           <Box mb={2}>
               <Button
                 variant="contained"
                 className={classes.button}
@@ -83,8 +102,8 @@ const Profile = ({ initialAppTheme, user }) => {
               >
                 View Bearer Token
               </Button>
-            </Box> */}
-            {/* {bearer_token && (
+            </Box>
+          {bearer_token && (
               <Box className={classes.result}>
                 <Typography variant="h6" className="muted">
                   Result
@@ -94,34 +113,18 @@ const Profile = ({ initialAppTheme, user }) => {
                 </Typography>
               </Box>
             )} */}
-          </Box>
-        </Layout>
-      </WithPageAuthRequired>
+        </Box>
+      </Layout>
     </>
   );
 };
 
-export default Profile;
+export default withPageAuthRequired(Profile);
 
 export async function getServerSideProps({ req, res }) {
-  // Here you can check authentication status directly before rendering the page,
-  // however the page would be a serverless function, which is more expensive and
-  // slower than a static page with client side authentication
-  const session = await auth0.getSession(req, res);
-  console.log("WE GOT SESSION", session.user);
-
-  if (!session || !session.user) {
-    res.writeHead(302, {
-      Location: "/api/auth/login",
-    });
-    res.end();
-    return;
-  }
-
   return {
     props: {
       initialAppTheme: req.cookies.appTheme || "light",
-      user: session.user,
     },
   };
 }
