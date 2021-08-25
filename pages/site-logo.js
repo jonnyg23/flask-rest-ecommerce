86009851d7c9055100041ac2e9db378ea7983a1a
jonnyg23 <jonguti23@outlook.com>
2021-08-25 11:09:29 -0600

    ➕ Add site logo

---
+++ pages/site-logo.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const SiteLogo = () => {
+  return (
+    <Image src="/site_logo.png" alt="Site Logo" width="250" height="250"/>
+  );
+};
+
+export default SiteLogo;
