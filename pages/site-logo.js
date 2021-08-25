f84af3d7a4801d9c33919f75d66b3c87c0c3561c
jonnyg23 <jonguti23@outlook.com>
2021-08-25 11:12:38 -0600

    🚮 Remove page

--- pages/site-logo.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-const SiteLogo = () => {
-  return (
-    <Image src="/site_logo.png" alt="Site Logo" width="250" height="250"/>
-  );
-};
-
-export default SiteLogo;
